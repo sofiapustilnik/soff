c694ee375249183822262f26ee8cee6b5cc24773
sofiapustilnik <160735165+sofiapustilnik@users.noreply.github.com>
2024-03-20 10:00:34 -0300

    Add files via upload

---
+++ sofm.py
@@ -0,0 +1,110 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================================================ RESTART: C:/Users/202201038421/sofiaaa.py ============================================================
+A media é 56.75
+Número 1: 20
+Número 2: 45
+Número 3: 69
+Número 4: 93
+Frase: Eu teria um desgosto profundo se faltasse o Flamengo no mundo
+Palavra: Flamengo
+
+============================================================ RESTART: C:/Users/202201038421/sofiaaa.py ============================================================
+A media é 56.75
+Número 1: 20
+Número 2: 45
+Número 3: 69
+Número 4: 93
+Frase: Eu teria um desgosto profundo se faltasse o Flamengo no mundo
+Palavra: Flamengo
+
+============================================================ RESTART: C:/Users/202201038421/sofiaaa.py ============================================================
+A media é 56.75
+o quadrado é 40
+Número 1: 20
+Número 2: 45
+Número 3: 69
+Número 4: 93
+Frase: Eu teria um desgosto profundo se faltasse o Flamengo no mundo
+Palavra: Flamengo
+
+============================================================ RESTART: C:/Users/202201038421/sofiaaa.py ============================================================
+A media é 56.75
+o quadrado é 40
+o dobro é 47
+Número 1: 20
+Número 2: 45
+Número 3: 69
+Número 4: 93
+Frase: Eu teria um desgosto profundo se faltasse o Flamengo no mundo
+Palavra: Flamengo
+
+============================================================ RESTART: C:/Users/202201038421/sofiaaa.py ============================================================
+A media é 56.75
+o quadrado é 40
+o dobro é 47
+Quantidade de letras da palavra: 8
+Número 1: 20
+Número 2: 45
+Número 3: 69
+Número 4: 93
+Frase: Eu teria um desgosto profundo se faltasse o Flamengo no mundo
+Palavra: Flamengo
+
+============================================================ RESTART: C:/Users/202201038421/sofiaaa.py ============================================================
+A media é 56.75
+o quadrado é 40
+o dobro é 47
+Quantidade de letras da palavra: 8
+Quantidade de espaços em branco na frase: 1
+Número 1: 20
+Número 2: 45
+Número 3: 69
+Número 4: 93
+Frase: Eu teria um desgosto profundo se faltasse o Flamengo no mundo
+Palavra: Flamengo
+
+============================================================ RESTART: C:/Users/202201038421/sofiaaa.py ============================================================
+A media é 56.75
+o quadrado é 40
+o dobro é 47
+Quantidade de letras da palavra: 8
+Quantidade de espaços em branco na frase: 1
+O primeiro número é maior que o segundo? False
+Número 1: 20
+Número 2: 45
+Número 3: 69
+Número 4: 93
+Frase: Eu teria um desgosto profundo se faltasse o Flamengo no mundo
+Palavra: Flamengo
+
+============================================================ RESTART: C:/Users/202201038421/sofiaaa.py ============================================================
+A media é 56.75
+o quadrado é 40
+o dobro é 47
+Quantidade de letras da palavra: 8
+Quantidade de espaços em branco na frase: 1
+O primeiro número é maior que o segundo? False
+O maior número é: 93
+Número 1: 20
+Número 2: 45
+Número 3: 69
+Número 4: 93
+Frase: Eu teria um desgosto profundo se faltasse o Flamengo no mundo
+Palavra: Flamengo
+
+============================================================ RESTART: C:/Users/202201038421/sofiaaa.py ============================================================
+A media é 56.75
+o quadrado é 40
+o dobro é 47
+Quantidade de letras da palavra: 8
+Quantidade de espaços em branco na frase: 1
+O primeiro número é maior que o segundo? False
+O maior número é: 93
+Número 1: 20
+Número 2: 45
+Número 3: 69
+Número 4: 93
+Frase: Eu teria um desgosto profundo se faltasse o Flamengo no mundo
+Palavra: Flamengo
